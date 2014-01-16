0638f350f32fc61c8611f7b7ce5570dfef30d194
JP Castro <jp.castro@am.sony.com>
2014-01-16 13:14:19 -0800

    Add initial prototype for What's New adapter

--- tests/activity-feeds/whats-new-demo.js
+++ tests/activity-feeds/whats-new-demo.js
@@ -1,19 +1,19 @@
-var assert = require("assert")
 describe("What's New", function(){
 	describe("Demo", function(){
-		test("should navigate the grid", function(){
-			assert.equal(true, true);
-
+		test("should navigate the grid and display story info", function(){
 			SkyNet.login("WHATS_NEW")
+
 			//SkyNet.login("STORE")
 
 			SkyNet.press("DOWN");
-			// Some assert here
+			console.log("\t%s".yellow, SkyNet.whatsNew.getTileInfo());
 
 			SkyNet.press("DOWN");
-			// Some other assert here
+			console.log("\t%s".yellow, SkyNet.whatsNew.getTileInfo());
 
 			SkyNet.press("DOWN");
+			console.log("\t%s".yellow, SkyNet.whatsNew.getTileInfo());
 		})
+
 	})
 })
