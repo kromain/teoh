4796a0bb9388272fbfbfd03057b4992cba5cf2ae
JP Castro <jp.castro@am.sony.com>
2014-01-17 11:57:06 -0800

    Add PS+ test

--- tests/store/ps-plus-purchase-flow.js
+++ tests/store/ps-plus-purchase-flow.js
@@ -1,11 +1,23 @@
 describe("Store", function(){
 	describe("PS+ purchase flow", function(){
 		test("should allow user to purchase 12 months sub", function(){
+
+			// Deeplink to the store
 			SkyNet.store.open();
-			SkyNet.wait(2000);
-			SkyNet.store.activeElementText.should.equal("Welcome");
+			SkyNet.wait(3000);
+
+			// Go back to global menu
+			SkyNet.press("TRIANGLE");
+			SkyNet.press("LEFT");
+			SkyNet.press("LEFT");
+			SkyNet.press("CROSS");
+
+			// Navigate in menu and verify entry labels
+			SkyNet.store.activeElementText.should.equal("Welcome", "Welcome entry is missing in store menu");
 			SkyNet.press("DOWN");
-			SkyNet.store.activeElementText.should.equal("PlayStation®Plus");
+			SkyNet.store.activeElementText.should.equal("PlayStation®Plus", "PS+ entry is missing in store menu");
+
+			// Navigate the caroussel
 			SkyNet.press("RIGHT");
 			SkyNet.wait(500);
 			SkyNet.press("RIGHT");
@@ -14,8 +26,9 @@ describe("Store", function(){
 			SkyNet.wait(500);
 			SkyNet.press("CROSS");
 
-			// TODO assert that button is Subscribe and that it goes to checkout?
+			SkyNet.wait(2000);
 
+			SkyNet.store.activeElementText.should.equal("Join PS+ $49.99", "PDP should show a Subscribe button with price")
 		});
 	});
 });
