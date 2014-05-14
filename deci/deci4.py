1e855a79e9531dcd119a0034b700ed3ecfc4e630
Steve Burnap <Steve.Burnap@am.sony.com>
2014-05-14 14:28:49 -0700

    Initial tests: scan controller input

---
+++ deci/deci4.py
@@ -0,0 +1,458 @@
+#!/usr/bin/env python
+
+import socket
+import array
+import struct
+import copy
+import datetime
+import time
+
+enable_logging = False
+
+def log(*args):
+    if enable_logging:
+        log_string = ''
+        for arg in args:
+            log_string += str(arg)
+        if (len(log_string)):
+            print log_string
+    return
+
+    
+def chunker(l,n):
+    for i in xrange(0,len(l),n):
+        yield l[i:i+n]
+
+def makewords(l):
+    for i in xrange(0,len(l),2):
+        s = l[i][2:]
+
+        if i+1 < len(l):
+            s += l[i+1][2:].zfill(2)
+
+        yield s.zfill(4)
+
+def ascdisp(b):
+    x = int(b,16)
+    
+    if x < 32 or x > 127:
+        return '.'
+    else:
+        return chr(x)
+         
+def make_dump(bytes):
+
+    if type(bytes) is str:
+        bytes = [ord(x) for x in bytes]
+
+    rc = ""
+    for addr, line in enumerate(chunker(map(hex, bytes), 16)):
+        rc += "%6s: %-39s %s\n" % (hex(addr * 16),
+                                  ' '.join(makewords(line)),
+                                  ''.join(map(ascdisp,line)))
+    return rc
+
+def log_bytes(text, bytes):
+
+    if enable_logging:
+        for addr, line in enumerate(chunker(map(hex, bytes), 16)):
+            print "%s %6s: %-39s %s" % (text,
+                                        hex(addr * 16),
+                                        ' '.join(makewords(line)),
+                                        ''.join(map(ascdisp,line)))
+
+
+
+def revlookup(dict, key):
+    return {dict[k]:k for k in dict.keys()}[key]
+
+def pad_len(bytes, multiple):
+    if (bytes / multiple) * multiple != bytes:
+        return ((bytes / multiple) + 1 ) * multiple
+    return bytes
+
+
+
+class ParseException(Exception):
+
+    def __init__(self, protocol, type):
+        self.protocol = protocol
+        self.type = type
+         
+    def __str__(self):
+        return "Unexpected message protocol %x type %x" % (self.protocol, self.type)
+
+class Deci:
+    recorddefs = {
+        "SceDeciHeader": [
+            {"type":'B', "length":1, "name":"version"},
+            {"type":"zeros", "length":3},
+            {"type":'<l', "length":4, "name":"length"},
+            {"type":'<L', "length":4, "name":"protocol"}
+        ],
+        "SceDeciUlpCmdHdr":[
+            {"type":'<H', "length":2, "name":"seqnumber"},
+            {"type":'<H', "length":2, "name":"fraginfo"},
+            {"type":'<l', "length":4, "name":"msgtype"}
+
+        ],
+        "SceDeciUlpResHdr":[
+            {"type":'<H', "length":2, "name":"seqnumber"},
+            {"type":'<H', "length":2, "name":"fraginfo"},
+            {"type":'<l', "length":4, "name":"msgtype"},
+            {"type":'<l', "length":4, "name":"result"},
+            {"type":'<l', "length":4, "name":"aux_error_code"}
+        ],
+        "SceDeciCommonConfig":[
+            {"type":'<l', "length":4, "name":"payload_length"},
+            {"type":'<l', "length":4, "name":"feature_number"},
+            {"type":'<l', "length":4, "name":"min_feature_number"},
+            {"type":'<l', "length":4, "name":"max_size_to_host"},
+            {"type":'<l', "length":4, "name":"max_size_to_target"},
+            {"type":'<l', "length":4, "name":"flags"}
+        ],
+        "SceNetmpConnectCmd":[
+            #{"type":'<l', "length":4, "name":"foo"},  # found by inspection
+            {"type":'zeros', "length":4},
+            {"type":"SceDeciStringUtf8", "name":"client_id"},
+            {"type":'<H', "length":2, "name":"udpport"},
+            {"type":"zeros", "length":2}
+        ],
+        "SceNetmpConnectRes":[
+            {"type":'<L', "length":4, "name":"netmp_key"},
+        ],
+        "SceNetmpRegisterCmd":[
+            {"type":'<L', "length":4, "name":"netmp_key"},
+            {"type":'<L', "length":4, "name":"reg_protocol"}
+        ],
+        "SceNetmpUnregisterCmd":[
+            {"type":'<L', "length":4, "name":"reg_protocol"}
+        ],
+        "SceCtrlpGetConfCmd":[
+            {"type":'<l', "length":4, "name":"in_buf_size"}
+        ],
+        "SceCtrlpDevices":[
+            {"type":'<L', "length":4, "name":"controller"}  # actually bitfield
+        ],
+        "SceCtrlpPlayCmd": [
+            {"type":'<l', "length":4, "name":"threshold"}
+        ],
+        "SceCtrlpData":[
+            {"type":'<L', "length":4, "name":"size"},
+            {"type":'<Q', "length":8, "name":"timestamp"},
+            {"type":'<L', "length":4, "name":"unionsize"},
+            {"type":'<L', "length":4, "name":"datatype"},
+            {"type":'<L', "length":4, "name":"datasize"},
+            {"type":'<L', "length":4, "name":"controller"},
+            {"type":'<L', "length":4, "name":"buttons"},
+            {"type":'<B', "length":1, "name":"lx"},
+            {"type":'<B', "length":1, "name":"ly"},
+            {"type":'<B', "length":1, "name":"rx"},
+            {"type":'<B', "length":1, "name":"ry"},
+            {"type":'<B', "length":1, "name":"l2"},
+            {"type":'<B', "length":1, "name":"r2"},
+            {"type":"zeros", "length":2},
+            {"type":'<L', "length":4, "name":"touchsize"},
+            {"type":'<Q', "length":8, "name":"timestamp2"},
+
+            {"type":"zeros", "length":4}  # list term, assume zero for now  (no list)
+        ]
+    }
+    sequence = 0x1234
+
+    def build_buffer(self, format, **kwargs):
+
+        for f in format:
+            if f["type"] == "SceDeciStringUtf8":
+                f["length"] = len(kwargs[f["name"]]) + 4
+
+        length = sum(f["length"] for f in format) 
+        buffer = array.array('B', [0]*length)
+
+        offset = 0
+        for f in format:
+            if f["type"] != "zeros":
+                if f["name"] == "length":
+                    val = length
+                else:
+                    val = kwargs[f["name"]]
+
+                if f["type"] != "SceDeciStringUtf8":
+                    struct.pack_into(f["type"], buffer, offset, val)
+                else:
+                    struct.pack_into("<l", buffer, offset, len(val))
+                    struct.pack_into("%ds" % len(val), buffer, offset+4, val)
+
+
+            offset += f["length"]
+
+        return buffer
+
+    def set_length(sef, buffer, format, length):
+        offset = 0
+        for f in format:
+            if f["type"] != "zeros":
+                if f["name"] == "length":
+                    struct.pack_into(f["type"], buffer, offset, length)
+
+            offset = offset + f["length"]
+        
+    def parse_buffer(self, buffer, format, res):
+        offset = 0
+        for f in format:
+            if f["type"] != "zeros":
+                res[f["name"]] = struct.unpack_from(f["type"], buffer, offset)[0]
+
+            offset += f["length"]
+
+        return buffer[offset:]
+
+    def make_deci_cmd_header(self, inbuff, message, protocol):
+
+        buffer = self.build_buffer(Deci.recorddefs["SceDeciHeader"], version=0x41, protocol=protocol)
+        buffer.extend(self.build_buffer(Deci.recorddefs["SceDeciUlpCmdHdr"], seqnumber=Deci.sequence, fraginfo=0, msgtype=message))
+        Deci.sequence += 1
+
+        if inbuff:
+            buffer.extend(inbuff)
+        self.set_length(buffer, Deci.recorddefs["SceDeciHeader"], len(buffer))
+
+        return buffer
+
+    def parse_header(self, buffer):
+        res = {}
+        buffer = self.parse_buffer(buffer, Deci.recorddefs["SceDeciHeader"], res)
+        buffer = self.parse_buffer(buffer, Deci.recorddefs["SceDeciUlpResHdr"], res)
+
+        return buffer, res
+
+
+    def sendrecv(self, stream, buffer):
+        log( "Send (%s):\n%s" % (stream, make_dump(buffer)) )
+        stream.send(buffer)
+        buffer = stream.recv(1024)
+        log( "Recv (%s):\n%s" % (stream, make_dump(buffer)) )
+        return buffer
+
+    def parse_assert(self, buffer, protocol, message):
+        buffer, res = self.parse_header(buffer)
+        if res["protocol"] != protocol or res["msgtype"] != message:
+            raise ParseException(res["protocol"], res["msgtype"])
+
+        return self.parse(res, buffer)[1]
+
+class NetmpProt(Deci):
+    SCE_NETMP_TYPE_GET_CONF_CMD = 0x0
+    SCE_NETMP_TYPE_GET_CONF_RES = 0x1
+    SCE_NETMP_TYPE_CONNECT_CMD = 0x2
+    SCE_NETMP_TYPE_CONNECT_RES = 0x3
+    SCE_NETMP_TYPE_DISCONNECT_CMD = 0x4
+    SCE_NETMP_TYPE_DISCONNECT_RES = 0x5
+    SCE_NETMP_TYPE_REGISTER_CMD = 0x6
+    SCE_NETMP_TYPE_REGISTER_RES = 0x7
+    SCE_NETMP_TYPE_UNREGISTER_CMD = 0x8
+    SCE_NETMP_TYPE_UNREGISTER_RES = 0x9
+    PROTOCOL = 0x40001000
+
+    def get_conf_cmd(self):
+        return self.make_deci_cmd_header(None, self.SCE_NETMP_TYPE_GET_CONF_CMD, self.PROTOCOL)
+
+    def get_conf_msg(self, stream):
+        buffer = self.sendrecv(stream, self.get_conf_cmd())
+        return self.parse_assert(buffer, self.PROTOCOL, self.SCE_NETMP_TYPE_GET_CONF_RES)
+
+    def connect_cmd(self, client_id, udpport):
+
+        buffer = self.build_buffer(Deci.recorddefs["SceNetmpConnectCmd"], client_id=client_id, udpport=udpport)
+        buffer = self.make_deci_cmd_header(buffer, self.SCE_NETMP_TYPE_CONNECT_CMD, self.PROTOCOL)
+
+        return buffer
+
+    def connect_msg(self, stream, client_id, udpport):
+        buffer = self.sendrecv(stream, self.connect_cmd(client_id='steve@43.138.12.225, foobar', udpport=0))
+        return self.parse_assert(buffer, self.PROTOCOL, self.SCE_NETMP_TYPE_CONNECT_RES)
+
+    def disconnect_cmd(self):
+
+        buffer = self.make_deci_cmd_header(None, self.SCE_NETMP_TYPE_DISCONNECT_CMD, self.PROTOCOL)
+
+        return buffer
+
+    def disconnect_msg(self, stream):
+        buffer = self.sendrecv(stream, self.disconnect_cmd())
+        return self.parse_assert(buffer, self.PROTOCOL, self.SCE_NETMP_TYPE_DISCONNECT_RES)
+
+    def register_cmd(self, netmp_key, reg_protocol):
+
+        buffer = self.build_buffer(Deci.recorddefs["SceNetmpRegisterCmd"], netmp_key=netmp_key, reg_protocol=reg_protocol)
+        buffer = self.make_deci_cmd_header(buffer, self.SCE_NETMP_TYPE_REGISTER_CMD, self.PROTOCOL)
+
+        return buffer
+
+    def register_msg(self, stream, netmp_key, reg_protocol):
+        buffer = self.sendrecv(stream, self.register_cmd(netmp_key, reg_protocol))
+        return self.parse_assert(buffer, self.PROTOCOL, self.SCE_NETMP_TYPE_REGISTER_RES)
+
+    def unregister_cmd(self, reg_protocol):
+
+        buffer = self.build_buffer(Deci.recorddefs["SceNetmpUnregisterCmd"], reg_protocol=reg_protocol)
+        buffer = self.make_deci_cmd_header(None, self.SCE_NETMP_TYPE_UNREGISTER_CMD, self.PROTOCOL)
+
+        return buffer
+
+    def unregister_msg(self, stream, reg_protocol):
+        buffer = self.sendrecv(stream, self.unregister_cmd(reg_protocol))
+        return self.parse_assert(buffer, self.PROTOCOL, self.SCE_NETMP_TYPE_UNREGISTER_RES)
+
+    def parse(self, res, buffer):
+        if res["msgtype"] == self.SCE_NETMP_TYPE_GET_CONF_RES:
+            buffer = self.parse_buffer(buffer, Deci.recorddefs["SceDeciCommonConfig"], res)
+        elif res["msgtype"] == self.SCE_NETMP_TYPE_CONNECT_RES:
+            buffer = self.parse_buffer(buffer, Deci.recorddefs["SceNetmpConnectRes"], res)
+        elif (res["msgtype"] == self.SCE_NETMP_TYPE_REGISTER_RES or 
+              res["msgtype"] == self.SCE_NETMP_TYPE_UNREGISTER_RES or
+              res["msgtype"] == self.SCE_NETMP_TYPE_DISCONNECT_RES):
+            pass
+
+        return buffer, res
+
+
+class CtrlpProt(Deci):
+    SCE_CTRLP_TYPE_GET_CONF_CMD = 0x0
+    SCE_CTRLP_TYPE_GET_CONF_RES = 0x1
+    SCE_CTRLP_TYPE_REC_START_CMD = 0x2
+    SCE_CTRLP_TYPE_REC_START_RES = 0x3
+    SCE_CTRLP_TYPE_REC_STOP_CMD = 0x4
+    SCE_CTRLP_TYPE_REC_STOP_RES = 0x5
+    SCE_CTRLP_TYPE_REC_DATA_NOTIFICATION = 0x7
+    SCE_CTRLP_TYPE_PLAY_START_CMD = 0x8
+    SCE_CTRLP_TYPE_PLAY_START_RES = 0x9
+    SCE_CTRLP_TYPE_PLAY_STOP_CMD = 0xA
+    SCE_CTRLP_TYPE_PLAY_STOP_RES = 0xB
+    SCE_CTRLP_TYPE_PLAY_DATA_CMD = 0xC
+    SCE_CTRLP_TYPE_PLAY_DATA_RES = 0xD
+    PROTOCOL = 0x0002c000
+
+    def get_conf_cmd(self):
+        return self.make_deci_cmd_header(None, self.SCE_CTRLP_TYPE_GET_CONF_CMD, self.PROTOCOL)
+
+    def get_conf_msg(self, stream):
+        buffer = self.sendrecv(stream, self.get_conf_cmd())
+        return self.parse_assert(buffer, self.PROTOCOL, self.SCE_CTRLP_TYPE_GET_CONF_RES)
+
+    def rec_start_cmd(self, controller=0xffffffff):
+        buffer = self.build_buffer(Deci.recorddefs["SceCtrlpDevices"], controller=controller)
+        return self.make_deci_cmd_header(buffer, self.SCE_CTRLP_TYPE_REC_START_CMD, self.PROTOCOL)
+
+    def rec_start_msg(self, stream, controller=0xffffffff):
+        buffer = self.sendrecv(stream, self.rec_start_cmd(controller))
+        return self.parse_assert(buffer, self.PROTOCOL, self.SCE_CTRLP_TYPE_REC_START_RES)
+
+    def rec_stop_cmd(self):
+        return self.make_deci_cmd_header(None, self.SCE_CTRLP_TYPE_REC_STOP_CMD, self.PROTOCOL)
+
+    def rec_stop_msg(self, stream):
+        buffer = self.sendrecv(stream, self.rec_stop_cmd())
+        return self.parse_assert(buffer, self.PROTOCOL, self.SCE_CTRLP_TYPE_REC_STOP_RES)
+
+    def parse(self, res, buffer):
+        if res["msgtype"] == self.SCE_CTRLP_TYPE_GET_CONF_RES:
+            buffer = self.parse_buffer(buffer, Deci.recorddefs["SceDeciCommonConfig"], res)
+            buffer = self.parse_buffer(buffer, Deci.recorddefs["SceCtrlpGetConfCmd"], res)
+        elif (res["msgtype"] == self.SCE_CTRLP_TYPE_REC_START_RES or 
+              res["msgtype"] == self.SCE_CTRLP_TYPE_REC_STOP_RES ):
+            pass
+
+        return buffer, res
+
+    def read_data(self, stream):
+        buffer = stream.recv(1024)
+        buffer, res = self.parse_header(buffer)
+
+        res["data"] = []
+        terminator = struct.unpack_from("<l", buffer, 0)[0]
+        while(terminator > 0):
+            resdata = {}
+            buffer = self.parse_buffer(buffer, Deci.recorddefs["SceCtrlpData"], resdata)
+            res["data"].append(resdata)
+            terminator = struct.unpack_from("<l", buffer, 0)[0]
+
+        return res
+
+
+
+class Netmp:
+    def __init__(self):
+        self.prot = NetmpProt()
+        self.stream1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.stream1.connect(("43.138.15.26", 8550))
+
+    def connect(self):
+        res = self.prot.connect_msg(self.stream1, client_id='steve@43.138.12.225, foobar', udpport=0)
+        self.netmp_key = res["netmp_key"]
+
+        #checkexception
+
+    def get_conf(self):
+        return self.prot.get_conf_msg(self.stream1)
+
+    def register_ctrlp(self):
+        self.stream2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.stream2.connect(("43.138.15.26", 8550))
+
+        res = self.prot.register_msg(self.stream2, netmp_key=self.netmp_key, reg_protocol=CtrlpProt.PROTOCOL)
+
+        #checkexception
+        return Ctrlp(self.stream2)
+
+    def unregister_ctrlp(self):
+        res = self.prot.unregister_msg(self.stream1, reg_protocol=CtrlpProt.PROTOCOL)
+
+        #checkexception
+        
+    def disconnect(self):
+        res = self.prot.disconnect_msg(self.stream1)
+
+        #checkexception
+
+class Ctrlp:
+    def __init__(self, stream):
+        self.prot = CtrlpProt()
+        self.stream = stream
+
+    def get_conf(self):
+        return self.prot.get_conf_msg(self.stream)
+
+    def rec_start(self):
+        self.prot.rec_start_msg(self.stream)
+
+    def rec_stop(self):
+        self.prot.rec_stop_msg(self.stream)
+
+    def read_data(self):
+        return self.prot.read_data(self.stream)
+
+netmp = Netmp()
+#print netmp.get_conf()
+
+netmp.connect()
+
+ctrlp = netmp.register_ctrlp()
+
+#print ctrlp.get_conf()
+
+ctrlp.rec_start()
+
+start = datetime.datetime.now();
+while( datetime.datetime.now() - start).seconds < 1:
+    data = ctrlp.read_data()
+    print len(data["data"])
+    for line in data["data"]:
+        print "Timestamp ", line["timestamp"], " buttons", hex(line["buttons"])
+
+ctrlp.rec_stop()
+
+netmp.unregister_ctrlp()
+
+netmp.disconnect()
+
