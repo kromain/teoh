6d67a12e5c58a16fa4cbf12e699f1e1dde3d0f98
Romain Pokrzywka <Romain.Pokrzywka@am.sony.com>
2014-06-06 18:34:41 -0700

    Add an entry point for Skynet which loads deci and psdriver

---
+++ skynet.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) 2014 Sony Network Entertainment Intl., all rights reserved.
+
+# FIXME make deci a proper module
+#import deci
+import psdriver
+import sys
+
+
+def main():
+    print("[08/29/1997 02:14] Skynet becomes self-aware.")
+    psd = psdriver.connect('43.138.15.55')
+    psd.quit()
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
