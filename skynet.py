ce8b3bb58eb1e12dbc902ec8e80ff6677ba0aba8
Romain Pokrzywka <romain.pokrzywka@am.sony.com>
2014-06-10 02:23:40 -0700

    Output psdriver server info during startup

--- skynet.py
+++ skynet.py
@@ -9,6 +9,9 @@ import sys
 
 def main():
     print("[08/29/1997 02:14] Skynet becomes self-aware.")
+    print("PSDriver server: {} (pid={})".format(psdriver.server.executable_path(),
+                                                psdriver.server.pid()))
+    print("PSDriver version: " + psdriver.__version__)
     psd = psdriver.connect_to_target('43.138.15.55')
     psd.quit()
     return 0
