13698ea0f44ddb785be8cf93ffcc6b2b2843cdf0
Romain Pokrzywka <Romain.Pokrzywka@am.sony.com>
2014-06-10 17:37:33 -0700

    Read the target IP address from the command line arguments

--- skynet.py
+++ skynet.py
@@ -12,7 +12,12 @@ def main():
     print("PSDriver server: {} (pid={})".format(psdriver.server.executable_path(),
                                                 psdriver.server.pid()))
     print("PSDriver version: " + psdriver.__version__)
-    psd = psdriver.connect_to_target('43.138.15.55')
+    if len(sys.argv) < 2:
+        print("No Target IP address specified, exiting.")
+        return 1
+
+    print("Connecting to target at {}...".format(sys.argv[1]))
+    psd = psdriver.server.connect(sys.argv[1])
     psd.quit()
     return 0
 
