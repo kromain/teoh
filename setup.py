3165841fbf551efc0adab011ef2d961dead2748a
Romain Pokrzywka <romain.pokrzywka@am.sony.com>
2014-07-03 14:02:53 -0700

    Create a beta1 release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='skynet',
-    version='0.1a1',
+    version='0.1b1',
     description='The PlayStation Remote Control',
     author='Romain Pokrzywka',
     author_email='Romain.Pokrzywka@am.sony.com',
