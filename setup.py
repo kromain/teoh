36b2c0a4cf98cc92c461ce0982a4e636e4414686
Romain Pokrzywka <Romain.Pokrzywka@am.sony.com>
2014-07-28 13:16:36 -0700

    Update the packaging script for 0.1b1
    - Include new skynet.osk package
    - Update psdriver binaries location
    - Create windows and unix (linux+mac) packages at once
      * use .zip for windows, .tar.gz for unix
      * split binaries by platform
      * don't create unix package from windows (permission issues for binaries)
    - Default to 'sdist' command if not specified on command line

--- setup.py
+++ setup.py
@@ -1,22 +1,54 @@
+import sys
 from setuptools import setup
 
-setup(
-    name='skynet',
-    version='0.1b1',
-    description='The PlayStation Remote Control',
-    author='Romain Pokrzywka',
-    author_email='Romain.Pokrzywka@am.sony.com',
-    license='LICENSE.txt',
-    url='https://qmjira.snei.sony.com/confluence/pages/viewpage.action?title=Skynet+Home&spaceKey=SKYN',
+package_platforms = ['win']
+if sys.platform == 'win32':
+    print("[NOTE] Linux/Mac package creation unsupported on Windows, only win32 package will be created.")
+else:
+    package_platforms.append('unix')
 
-    packages=['skynet', 'skynet.deci', 'skynet.psdriver'],
-    package_data = {
-        # Also package and distribute the psdriver executables in skynet/psdriver/bin
-        'skynet.psdriver': ['bin/*'],
-    },
 
-    install_requires=[
-        "selenium >= 2.42.1",
-        "pytest >= 2.5.2",
-    ],
-)
+def setup_platform(package_platform):
+    cmd_args = sys.argv[1:]
+    if len(cmd_args) == 0:
+        cmd_args.append("sdist")
+
+    if package_platform == 'win':
+        bin_dir = ['bin/win32/*']
+        cmd_args.append("--formats=zip")
+    else:
+        bin_dir = ['bin/linux/*', 'bin/darwin/*']
+        cmd_args.append("--formats=gztar")
+
+    setup(
+        name='skynet',
+        version='0.1b1',
+        description='The PlayStation Remote Control',
+        author='Romain Pokrzywka',
+        author_email='Romain.Pokrzywka@am.sony.com',
+        license='LICENSE.txt',
+        url='https://qmjira.snei.sony.com/confluence/pages/viewpage.action?title=Skynet+Home&spaceKey=SKYN',
+
+        packages=[
+            'skynet',
+            'skynet.deci',
+            'skynet.osk',
+            'skynet.psdriver',
+        ],
+
+        package_data={
+            # Also package and distribute the psdriver executables in skynet/psdriver/bin/<platform>
+            'skynet.psdriver': bin_dir,
+        },
+
+        install_requires=[
+            "selenium >= 2.42.1",
+            "pytest >= 2.5.2",
+        ],
+
+        script_args=cmd_args
+    )
+
+
+for p in package_platforms:
+    setup_platform(p)
