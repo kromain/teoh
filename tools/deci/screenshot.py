c6472aaf80a08b03facdb364ac03381d29b6b143
Steve Burnap <Steve.Burnap@am.sony.com>
2014-10-02 15:35:10 -0700

    Changed from test to simple screenshot script

--- tools/deci/screenshot.py
+++ tools/deci/screenshot.py
@@ -7,17 +7,7 @@ if __name__ == "__main__":
     try:
         with Info(ip=sys.argv[1]) as info:
 
-            if info.is_user_signed_in("steve-e1"):
-                print("Yes")
-            else:
-                print("No")
-
-            print("Read game")
-            info.get_pict("GAME" + sys.argv[2])
-            print("Read system")
-            info.get_pict("SYSTEM" + sys.argv[2])
-            print("Read auto")
-            info.get_pict("AUTO" + sys.argv[2])
+            info.get_pict(sys.argv[2])
 
 
     except IndexError:
