1b911ea1df265c0e16ae83eb5c27d8a477182592
Romain Pokrzywka <Romain.Pokrzywka@am.sony.com>
2014-10-02 17:18:37 -0700

    pytest-ify test_screenshot
    Split tga and png tests, parametrize the latter by extension to throw in
    jpg extensions as well, and mark it as skipped if the Pillow package isn't
    installed.

--- tests/deci/test_screenshot.py
+++ tests/deci/test_screenshot.py
@@ -1,42 +1,47 @@
 #!/usr/bin/env python3
+#
+# Copyright (c) 2014 Sony Network Entertainment Intl., all rights reserved.
+
 import os
-import time
 import pytest
 
 from skynet.deci.info import Info
 import conftest
 
-test_target_ip = conftest.target_ip
-
-class TestScreenshot:
-
-    def setup(self):
-        pass
-
-    def teardown(self):
-        pass
+pil_loaded = False
+try:
+    from PIL import Image
+    pil_loaded = True
+except:
+    pass
 
 
-    def test_screenshot(self):
-        with Info(ip=test_target_ip) as info:
-
-            info.get_pict("TEST.tga")
-            assert(os.path.getsize("TEST.tga") == 8294428)
-            os.remove("TEST.tga")
-
-            info.get_pict("TEST")
-            assert(os.path.getsize("TEST.tga") == 8294428)
-            os.remove("TEST.tga")
-            
-            info.get_pict("TEST.png")
-            assert(os.stat("TEST.png"))
-            with pytest.raises(FileNotFoundError):
-                os.stat("TEST.tga")
-            os.remove("TEST.png")
-
-            assert(len([block for block in info.get_pict_blocks()]) == 127)
+@pytest.fixture(scope="module")
+def info(request):
+    i = Info(ip=conftest.target_ip)
+    i.start()
+    request.addfinalizer(i.stop)
+    return i
 
 
+def test_tga_screenshot(info):
+    info.get_pict("TEST.tga")
+    assert(os.path.getsize("TEST.tga") == 8294428)
+    os.remove("TEST.tga")
+
+    info.get_pict("TEST")
+    assert(os.path.getsize("TEST.tga") == 8294428)
+    os.remove("TEST.tga")
+
+    assert len([block for block in info.get_pict_blocks()]) == 127
 
 
-
+@pytest.mark.skipif(not pil_loaded, reason="requires Pillow package")
+@pytest.mark.parametrize("extension", ["png", "jpg"])
+def test_encoded_screenshot(info, extension):
+    filename = "TEST.{}".format(extension)
+    info.get_pict(filename)
+    assert(os.stat(filename))
+    with pytest.raises(FileNotFoundError):
+        os.stat("TEST.tga")
+    os.remove(filename)
