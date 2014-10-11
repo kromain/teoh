e05f7d9c0d668e15b11f483c98c601528cdea247
Romain Pokrzywka <Romain.Pokrzywka@am.sony.com>
2014-10-10 17:37:44 -0700

    Introduce the Mantis plugin for automatically distributing tests among devkits
    It is based on the xdist plugin, with customizations for our use case.
    This was needed as the hooks provided weren't enough to customize the
    behavior to our requirements.
    
    Note that we're still using most of the xdist infrastructure, we're just
    customizing the way nodes are initialized and in the future replacing the
    LoadScheduler with our own.

--- testrunner/mantisrunner.py
+++ testrunner/mantisrunner.py
@@ -3,19 +3,12 @@ import sys
 
 import pytest
 import _pytest.config
-from skynet.config import Config
-
-
-def extract_node_count(config):
-    return str(len(config.target_configs()))
 
 
 def main():
     args = sys.argv[1:]
 
-    skynet_config = Config()
-    skynet_config.parse_json()
-    args.extend(["-n", extract_node_count(skynet_config)])
+    args.extend(["-p", "mantisplugin"])
     args.extend(["--ip=1.2.3.4"])
 
     pluginmanager = _pytest.config.get_plugin_manager()
@@ -24,9 +17,10 @@ def main():
         exitstatus = pluginmanager.hook.pytest_cmdline_main(config=config)
     except pytest.UsageError:
         e = sys.exc_info()[1]
-        sys.stderr.write("ERROR: %s\n" %(e.args[0],))
+        sys.stderr.write("ERROR: %s\n" % (e.args[0],))
         exitstatus = -1
     return exitstatus
+
 
 if __name__ == "__main__":
     main()
