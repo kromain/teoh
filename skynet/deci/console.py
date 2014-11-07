1ba869b5791345c883c364ba9e4ecd75bcb97b48
Romain Pokrzywka <romain.pokrzywka@am.sony.com>
2014-11-06 22:30:06 -0800

    Add API docs for Console

--- skynet/deci/console.py
+++ skynet/deci/console.py
@@ -4,6 +4,16 @@ import time
 from .deci4 import NetmpManager,Ttyp
 
 class Console(NetmpManager):
+    """
+    The interface to the TTY console on the target.
+
+    This class allows retrieving the global output from the TTY console on the target. This is the same output that
+    the Neighborhood Console Output retrieves.
+
+    The output is retrieved line by line as long as there's data available. If there's no data available,
+    :meth:`read()` will return immediately without blocking while :meth:`readsync()` will block until a line is
+    available.
+    """
     def __init__(self, ip):
         self.ip = ip
 
@@ -29,6 +39,10 @@ class Console(NetmpManager):
         return self.ttyp.get_port_states()
 
     def read(self):
+        """ Returns the next buffered line of output from the TTY console, or return immediately if there's no data.
+
+        :return: One line of output as String, or None if no TTY data is immediately available.
+        """
         res = self.ttyp.read()
         if res:
             # It would be better to check the message type somehow
@@ -40,6 +54,10 @@ class Console(NetmpManager):
         return None
 
     def readsync(self):
+        """ Returns the next buffered line of output from the TTY console, or wait until more data becomes available.
+
+        :return: One line of output as String, whenever the next one is available.
+        """
         while True:
             res = self.ttyp.readsync()
 
